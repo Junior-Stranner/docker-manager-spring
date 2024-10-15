273a3e6c83b3a33f02bfdf5582970aa2892b97cd
Junior-Stranner <JuniorStranner@gmail.com>
2024-10-15 16:51:15 -0300

    start coding

---
+++ docker-manager/src/main/java/br/com/judev/dockermanager/DockerManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.judev.dockermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerManagerApplication.class, args);
+    }
+
+}
---
+++ docker-manager/src/test/java/br/com/judev/dockermanager/DockerManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.judev.dockermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
